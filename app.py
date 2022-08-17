4425eeb3f477ef2cde28641ebaa49128de1c590d
Teoh TeikToe <ttt@mensa.org.sg>
2022-08-17 12:04:44 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+from flask import Flask,request,render_template
+
+
+# In[16]:
+
+
+import joblib
+
+
+# In[17]:
+
+
+app = Flask(__name__)
+
+
+# In[18]:
+
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        rates = float(request.form.get("rates"))
+        model1 = joblib.load("regression")
+        r1 = model1.predict([[rates]])
+        model2 = joblib.load("tree")
+        r2 = model2.predict([[rates]])
+        print(rates)
+        return(render_template("index.html",result1=r1,result2=r2))
+    else:
+        return(render_template("index.html",result1="waiting",result2="waiting"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
